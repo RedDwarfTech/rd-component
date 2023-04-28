192a87d3baf1e21d61146b5122549d297641305c
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-04-28 18:55:19 +0800

    feat: add reducer export

--- src/common/combineReducer.ts
+++ src/common/combineReducer.ts
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
+import pay from '@/reducer/pay/PayReducer';
+import iapproduct from '@/reducer/iapproduct/IapProductReducer';
 
 const rootReducer = combineReducers({
-  
+  pay,
+  iapproduct
 })
 
 export default rootReducer;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import CommonPage from "./component/demo/CommonPage";
 import Goods from "./component/good/Goods";
 import Pay from "./component/pay/Pay";
+import RdReducer from "./common/combineReducer";
 
-export { CommonPage, Goods, Pay }
+export { CommonPage, Goods, Pay, RdReducer }
