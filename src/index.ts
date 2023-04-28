9e31fe8c034118b7194965df2efdf94bb8f0dcf9
jiang qiang <jiangtingqiang@gmail.com>
2023-04-28 22:12:14 +0800

    feat: add type for store

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
 import CommonPage from "./component/demo/CommonPage";
 import Goods from "./component/good/Goods";
 import Pay from "./component/pay/Pay";
-import RdReducer from "./common/combineReducer";
+import rdRootReducer from "./common/combineReducer";
+import CommonPageFunction from "./component/demo/CommonPageFunction";
+import CommonPageCon from "./component/demo/CommonPageCon";
+import withConnect from "./component/hoc/withConnect";
+import store from "./store/store";
 
-export { CommonPage, Goods, Pay, RdReducer }
+export { 
+    CommonPage, 
+    Goods, 
+    Pay, 
+    rdRootReducer,
+    CommonPageFunction,
+    CommonPageCon,
+    withConnect,
+    store 
+}
