1e3d29d419a44463613cd2083d268a7c47f8edab
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-04-29 17:57:39 +0800

    feat: add footer

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import CommonPage from "./component/demo/CommonPage";
 import Goods from "./component/good/Goods";
 import Pay from "./component/pay/Pay";
 import RdReducer from "./common/combineReducer";
+import Footer from "./component/layout/footer/Footer";
 
-export { CommonPage, Goods, Pay, RdReducer }
+export { CommonPage, Goods, Pay, RdReducer,Footer }
