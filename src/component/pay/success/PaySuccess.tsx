61b7a13cba9e0c92590b3d3bbd29e85f6676af9e
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-05-12 23:27:52 +0800

    refactor: reset the pay success

--- src/component/pay/success/PaySuccess.tsx
+++ src/component/pay/success/PaySuccess.tsx
@@ -1,34 +1,15 @@
 import paySuccessIcon from "../../../assets/icons/pay/pay-success.png";
 import "./PaySuccess.css";
-import { useLocation } from 'react-router-dom';
-import queryString from 'query-string';
 
 const PaySuccess: React.FC = () => {
-
-    const location = useLocation();
-    if (location.search == null) {
-        return (<div className="pay-success-body">
-            <h1>支付失败！</h1>
-        </div>);
-    }
-    // 回调的链接可能是这样的格式https://read.poemhub.top/product/pay/success?orderId=619464963527516160&amp;payAmount=0.01&amp;sign_type=RSA2&amp;timestamp=2023-03-09%2002:21:56&amp;version=1.0
-    // &amp;是为了兼容老旧的浏览器
-    // 现代浏览器已经不再需要将 & 编码为 &amp;，但一些较旧版本的浏览器和遗留系统可能仍然需要这样做。
-    const parsed = queryString.parse(location.search.replace(/&amp;/g, '&'));
-    if (parsed != null && parsed.orderId && parsed.payAmount) {
-        return (
-            <div className="pay-success-body">
-                <div className="pay-success-indicator">
-                    <img src={paySuccessIcon}></img>
-                </div>
-                <div className="pay-success-tip">支付成功</div>
+    return (
+        <div className="pay-success-body">
+            <div className="pay-success-indicator">
+                <img src={paySuccessIcon}></img>
             </div>
-        );
-    } else {
-        return (<div className="pay-success-body">
-            <h1>支付失败！</h1>
-        </div>);
-    }
+            <div className="pay-success-tip">支付成功</div>
+        </div>
+    );
 }
 
 export default PaySuccess;
