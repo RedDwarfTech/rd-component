b44fef14048b9813388498bd47a8a65bfdee110c
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-05-28 15:13:13 +0800

    feat: fix the compile issue

--- src/service/goods/GoodsService.ts
+++ src/service/goods/GoodsService.ts
@@ -1,6 +1,5 @@
-import { requestWithAction } from '@/common/XHRClient';
-import { ProductReq } from "js-wheel/dist/src/model/product/ProductReq";
 import { getIapProductsAction } from '@/action/iapproduct/IapProductAction';
+import XHRClient from '@/common/XHRClient';
 import { AnyAction, Store } from 'redux';
 
 export function doGetIapProduct(store: Store<any, AnyAction>) {
@@ -9,5 +8,5 @@ export function doGetIapProduct(store: Store<any, AnyAction>) {
         url: '/post/product/v1/list',
         headers: {'Content-Type': 'application/json'}
     };
-    return requestWithAction(config, getIapProductsAction,store);
+    return XHRClient.requestWithAction(config, getIapProductsAction,store);
 }
