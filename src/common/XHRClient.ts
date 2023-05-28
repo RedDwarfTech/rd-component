5e587a34fa5503b3282ed8067ac2587a81a4c45c
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-05-28 15:12:01 +0800

    feat: add type for axios response

--- src/common/XHRClient.ts
+++ src/common/XHRClient.ts
@@ -1,7 +1,8 @@
 import axios, { AxiosRequestConfig, AxiosResponse, InternalAxiosRequestConfig } from 'axios';
 import { v4 as uuidv4 } from 'uuid';
-import { RequestHandler, ResponseCode, WheelGlobal } from 'js-wheel';
+import { REST, RequestHandler, ResponseCode, ResponseHandler, WheelGlobal } from 'js-wheel';
 import { AnyAction, Store } from 'redux';
+import { message } from 'antd';
 
 let isRefreshing = false
 let pendingRequestsQueue: Array<any> = [];
@@ -14,7 +15,7 @@ instance.defaults.headers.post['Content-Type'] = 'application/json'
 
 export const XHRClient = {
   requestWithoutAction: async (config: AxiosRequestConfig): Promise<any> => {
-    addRequiredHeaders();
+    XHRClient.addRequiredHeaders();
     try {
       const response = await instance(config);
       return response;
@@ -34,92 +35,95 @@ export const XHRClient = {
       'x-action': actionType
     };
     config.headers = Object.assign({}, config.headers, generalHeader);
-    addRequiredHeaders(store);
-    return instance(config).then((response: { data: { result: any; }; }) => {
-      const data = response.data.result;
-      const localAction = {
-        type: actionType,
-        data: data
-      };
-      store.dispatch(localAction);
-      return response.data;
+    XHRClient.addRequiredHeaders(store);
+    return instance(config).then((response: AxiosResponse) => {
+      const appResponse: REST.ApiResponse = response.data;
+      if (ResponseHandler.responseSuccess(appResponse)) {
+        const data = appResponse.result;
+        const localAction = {
+          type: actionType,
+          data: data
+        };
+        store.dispatch(localAction);
+        return appResponse;
+      } else {
+        message.error(appResponse.msg)
+      }
     }).catch((error: any) => {
       console.error(error);
     });
+  },
+  requestWithAction: (config: AxiosRequestConfig, action: any, store: Store<any, AnyAction>) => {
+    const actionJson = action({}).type;
+    const generalHeader = {
+      'x-action': actionJson
+    };
+    config.headers = Object.assign({}, config.headers, generalHeader);
+    XHRClient.addRequiredHeaders(store);
+    return instance(config).then((response: { data: { result: any; }; }) => {
+      const data = response.data.result;
+      store.dispatch(action(data));
+      return response.data;
+    }
+    ).catch(
+      (error: any) => {
+        console.error(error);
+      }
+    );
+  },
+  addRequiredHeaders: (store?: Store<any, AnyAction>) => {
+    instance.interceptors.request.use((request) => {
+      const accessToken = localStorage.getItem(WheelGlobal.ACCESS_TOKEN_NAME);
+      accessToken && (request.headers['x-access-token'] = accessToken);
+      request.headers['x-request-id'] = uuidv4();
+      return request
+    },
+      (error: any) => {
+        return Promise.reject(error)
+      }
+    )
+
+    instance.interceptors.response.use((response: AxiosResponse<any, any>) => {
+      const originalRequest: InternalAxiosRequestConfig<any> = response.config;
+      if (isRefreshing) {
+        pendingRequestsQueue.push(originalRequest);
+      }
+      if (!isRefreshing) {
+        if (response.data.resultCode === ResponseCode.ACCESS_TOKEN_EXPIRED
+          || response.data.resultCode === ResponseCode.ACCESS_TOKEN_INVALID) {
+          pendingRequestsQueue.push(originalRequest);
+          isRefreshing = true;
+          // refresh the access token
+          RequestHandler.handleWebAccessTokenExpire()
+            .then((data: any) => {
+              isRefreshing = false;
+              pendingRequestsQueue.forEach((request) => {
+                const accessToken = localStorage.getItem(WheelGlobal.ACCESS_TOKEN_NAME);
+                request.headers['x-access-token'] = accessToken;
+                request.headers['x-request-id'] = uuidv4();
+                instance(request).then((resp: any) => {
+                  if (!store) return;
+                  const actionType = response.config.headers['x-action'];
+                  if (actionType) {
+                    const data = resp.data.result;
+                    const action = {
+                      type: actionType,
+                      data: data
+                    };
+                    // change the state to make it render the UI
+                    store.dispatch(action);
+                  }
+                });
+              });
+              pendingRequestsQueue = [];
+            });
+        }
+      }
+      return response;
+    },
+      (error: any) => { return Promise.reject(error) }
+    )
   }
 }
 
 export default XHRClient;
-
-export const addRequiredHeaders = (store?: Store<any, AnyAction>) => {
-  instance.interceptors.request.use((request) => {
-    const accessToken = localStorage.getItem(WheelGlobal.ACCESS_TOKEN_NAME);
-    accessToken && (request.headers['x-access-token'] = accessToken);
-    request.headers['x-request-id'] = uuidv4();
-    return request
-  },
-    (error: any) => {
-      return Promise.reject(error)
-    }
-  )
-
-  instance.interceptors.response.use((response: AxiosResponse<any, any>) => {
-    const originalRequest: InternalAxiosRequestConfig<any> = response.config;
-    if (isRefreshing) {
-      pendingRequestsQueue.push(originalRequest);
-    }
-    if (!isRefreshing) {
-      if (response.data.resultCode === ResponseCode.ACCESS_TOKEN_EXPIRED
-        || response.data.resultCode === ResponseCode.ACCESS_TOKEN_INVALID) {
-        pendingRequestsQueue.push(originalRequest);
-        isRefreshing = true;
-        // refresh the access token
-        RequestHandler.handleWebAccessTokenExpire()
-          .then((data: any) => {
-            isRefreshing = false;
-            pendingRequestsQueue.forEach((request) => {
-              const accessToken = localStorage.getItem(WheelGlobal.ACCESS_TOKEN_NAME);
-              request.headers['x-access-token'] = accessToken;
-              request.headers['x-request-id'] = uuidv4();
-              instance(request).then((resp: any) => {
-                if (!store) return;
-                const actionType = response.config.headers['x-action'];
-                if (actionType) {
-                  const data = resp.data.result;
-                  const action = {
-                    type: actionType,
-                    data: data
-                  };
-                  // change the state to make it render the UI
-                  store.dispatch(action);
-                }
-              });
-            });
-            pendingRequestsQueue = [];
-          });
-      }
-    }
-    return response;
-  },
-    (error: any) => { return Promise.reject(error) }
-  )
-}
-
-export function requestWithAction(config: AxiosRequestConfig, action: any, store: Store<any, AnyAction>) {
-  const actionJson = action({}).type;
-  const generalHeader = {
-    'x-action': actionJson
-  };
-  config.headers = Object.assign({}, config.headers, generalHeader);
-  addRequiredHeaders(store);
-  return instance(config).then((response: { data: { result: any; }; }) => {
-    const data = response.data.result;
-    store.dispatch(action(data));
-    return response.data;
-  }
-  ).catch(
-    (error: any) => {
-      console.error(error);
-    }
-  );
-}
