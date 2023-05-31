87921cf0244e5ef01af1e686f03a67e9600810fa
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-05-31 12:45:03 +0800

    feat: add url when send sse

--- src/service/sse/SseClientService.ts
+++ src/service/sse/SseClientService.ts
@@ -4,22 +4,22 @@ import { v4 as uuid } from 'uuid';
 import { EventSourcePolyfill } from 'event-source-polyfill';
 
 export const SseClientService = {
-    doAskPreCheck: (params: ChatAsk, onSseMessage: (msg: string, eventSource: EventSourcePolyfill) => void) => {
+    doAskPreCheck: (params: ChatAsk, onSseMessage: (msg: string, eventSource: EventSourcePolyfill) => void, url: string) => {
         if (AuthHandler.isTokenNeedRefresh(60)) {
             RequestHandler.handleWebAccessTokenExpire()
                 .then((data) => {
-                    SseClientService.doSseChatAsk(params, onSseMessage);
+                    SseClientService.doSseChatAsk(params, onSseMessage,url);
                 });
         } else {
-            SseClientService.doSseChatAsk(params, onSseMessage);
+            SseClientService.doSseChatAsk(params, onSseMessage,url);
         }
     },
-    doSseChatAsk: (params: ChatAsk, onSseMessage: (msg: string, eventSource: EventSourcePolyfill) => void) => {
+    doSseChatAsk: (params: ChatAsk, onSseMessage: (msg: string, eventSource: EventSourcePolyfill) => void, url: string) => {
         let eventSource: EventSourcePolyfill;
         const accessToken = localStorage.getItem("x-access-token");
         // https://stackoverflow.com/questions/6623232/eventsource-and-basic-http-authentication
         var queryString = Object.keys(params).map(key => key + '=' + params[key as keyof ChatAsk]).join('&');
-        eventSource = new EventSourcePolyfill('/ai/stream/chat/ask?' + queryString, {
+        eventSource = new EventSourcePolyfill(url + '?' + queryString, {
             headers: {
                 'x-access-token': accessToken ?? "",
                 'x-request-id': uuid(),
