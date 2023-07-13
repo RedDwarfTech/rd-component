d3376507cfa597f17a0799d60c570ea96553fe23
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-07-13 17:27:06 +0800

    feat: tweak the web footer

--- src/component/layout/footer/Footer.tsx
+++ src/component/layout/footer/Footer.tsx
@@ -1,26 +1,20 @@
 import './Footer.css';
 import React from "react";
-import { Col, Divider, Row } from 'antd';
 import policeLogo from "../../../assets/footer/gwab.webp";
-
 const Footer: React.FC = () => {
 
-  let yearNow = new Date().toISOString().substr(0, 4);
+  let yearNow = new Date().toISOString().substr(0,4);
 
   return (
     <div className="App-footer-float">
       <div id="footer" className="App-footer">
-        <Divider></Divider>
-        <Row className="custom-row" justify="center" align="middle">
-          <Col>
-            <div className="App-footer-div">&copy;2021-{yearNow} RedDwarf 重庆红矮星科技有限公司 <a href="http://beian.miit.gov.cn/">渝ICP备2021003460号-1</a> <a href="mailto:jiangxiaoqiang@zqhaxkjyxgs.wecom.work">联系我们</a></div>
-          </Col>
-        </Row>
-        <Row className="custom-row" justify="center" align="middle">
-          <Col>
-            <div className='App-footer-div'><img src={policeLogo}></img><a href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=50022502000614"> 渝公网安备 50022502000614 号</a></div>
-          </Col>
-        </Row>
+        <div className="App-footer-divider"></div>
+        <div className="custom-row">
+          <div className="App-footer-div">&copy;2021-{yearNow} RedDwarf 重庆红矮星科技有限公司 <a href="http://beian.miit.gov.cn/">渝ICP备2021003460号-1</a> <a href="mailto:jiangxiaoqiang@zqhaxkjyxgs.wecom.work">联系我们</a></div>
+        </div>
+        <div className="custom-row">
+          <div className="App-footer-div"><img src={policeLogo}></img><a href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=50022502000614"> 渝公网安备 50022502000614 号</a></div>
+        </div>
       </div>
     </div>
   );
