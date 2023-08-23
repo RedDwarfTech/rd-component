a0013569e4bd7ae7b1906d86290e91315b25ca81
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-08-23 17:17:30 +0800

    feat: add current user load return

--- src/service/user/UserService.ts
+++ src/service/user/UserService.ts
@@ -112,13 +112,19 @@ export const UserService = {
         const actionTypeString: string = UserActionType[UserActionType.USER_UNBIND];
         return XHRClient.requestWithActionType(config, actionTypeString, store);
     },
-    loadCurrUser: (force: boolean, url: string) => {
+    loadCurrUser: (force: boolean, url: string): UserModel => {
+        let userInfo: UserModel = {
+            nickname: 'unkown',
+            autoRenewProductExpireTimeMs: 0,
+            thirdBind: []
+        };
         if (force) {
             UserService.getCurrUser(url).then((data: any) => {
                 if (ResponseHandler.responseSuccess(data)) {
                     const uid = data.result.userId;
                     if (uid) {
                         localStorage.setItem("userInfo", JSON.stringify(data.result));
+                        userInfo = uid;
                     }
                 }
             });
@@ -130,11 +136,13 @@ export const UserService = {
                         const uid = data.result.userId;
                         if (uid) {
                             localStorage.setItem("userInfo", JSON.stringify(data.result));
+                            userInfo = uid;
                         }
                     }
                 });
             }
         }
+        return userInfo;
     }
 }
 
